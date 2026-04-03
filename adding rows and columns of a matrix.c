ffbe3ec63197c41b3df04c422aef03f32136106a
stutij121 <stutij121@gmail.com>
2026-04-03 09:50:17 +0530

    Add files via upload

---
+++ adding rows and columns of a matrix.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main ()
+{
+    int a[2][2],i,j;
+    int m,n;
+
+    printf("enter the number of rows and columns of the matrix");
+    scanf("%d %d,&m,&n");
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+
+        }
+    }
+    //row sum //
+    printf("\nsum of the rows:\n");
+    for(i=0;i<m;i++)
+    {
+        int sum=0;
+        for(j=0;j<n;j++)
+        {
+         sum=sum+a[i][j];
+        }
+        printf("row %d = %d\n,i+1,sum");
+    }
+    //column sum//
+    printf("\n sum of columns\n");
+    for(j=0;j<n;j++)
+    {
+        int sum =0;
+        for(i=0;i<m;i++)
+        {
+            sum=sum+a[i][j];
+        }
+        printf("column %d = %d\n",i+1,sum);
+
+    }
+    return 0;
+
+}
