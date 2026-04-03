be72cbe6256894c3067f1aab5fbec8d0d18ec0ba
stutij121 <stutij121@gmail.com>
2026-04-03 12:23:47 +0530

    Add files via upload

---
+++ decimal to binary.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main ()
+{
+    int n,i,binary;
+    printf("enter a decimal number");
+    scanf("%d",& n);
+    
+    while(n>0){
+        binary[i]=n%2;
+        
+        n=n/2;
+        
+    i++;
+    }
+    printf("binary=");
+    
+for(intj=i-`;j>=0;j--){
+    printf("%d"binary[j]);
+}
+return 0;
+}
